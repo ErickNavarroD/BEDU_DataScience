640214f1b2f412f9088e1fb1dab00035382fa272
Erick Navarro <ericknadel98@ciencias.unam.mx>
2021-01-28 02:14:35 +0000

    Reto 1

--- R/Sesion7.R
+++ R/Sesion7.R
@@ -1,1 +1,33 @@
-#
+#Reto 1 
+#Queries
+
+install.packages("DBI")
+install.packages("RMySQL")
+install.packages("dplyr")
+install.packages("ggplot2")
+
+library(DBI)
+library(RMySQL)
+library(dplyr)
+library(ggplot2)
+
+#Nos conectamos a una base de datos
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+# Si no se arrojaron errores por parte de R, vamos a explorar la BDD
+dbListTables(MyDataBase)
+
+# Ahora si se quieren desplegar los campos o variables que contiene la tabla 
+# City se hará lo siguiente
+dbListFields(MyDataBase, 'CountryLanguage')
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage where Language = 'Spanish'")
+
+ggplot(DataDB, aes(x = CountryCode, y = Percentage))+
+  geom_bar(stat = "identity") + 
+  theme_classic() + 
+  ggtitle("Porcentaje de hablantes de español por pais")
